070794be6d72722db89765e0d57b6333984a6543
Anna Chernyshova <anna.chernyshova91@gmail.com>
2018-02-27 13:16:29 +0300

    updated Akita version to 3.1.0 with CustomDriverProvider

--- src/main/java/pages/GitHubMainPage.java
+++ src/main/java/pages/GitHubMainPage.java
@@ -17,8 +17,8 @@ package pages;
 
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.support.FindBy;
+import ru.alfabank.alfatest.cucumber.annotations.Name;
 import ru.alfabank.alfatest.cucumber.api.AkitaPage;
-import ru.alfabank.alfatest.cucumber.api.AkitaPage.Name;
 
 @Name("Страница входа GitHub")
 public class GitHubMainPage extends AkitaPage {
--- src/main/java/pages/RepositoriesPage.java
+++ src/main/java/pages/RepositoriesPage.java
@@ -17,8 +17,8 @@ package pages;
 
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.support.FindBy;
+import ru.alfabank.alfatest.cucumber.annotations.Name;
 import ru.alfabank.alfatest.cucumber.api.AkitaPage;
-import ru.alfabank.alfatest.cucumber.api.AkitaPage.Name;
 
 import java.util.List;
 
